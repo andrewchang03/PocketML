d4c867310609e2adfd46e9bacff8923c79123185
Andrew Chang <andrewchang@dhcp-vl2041-17439.eduroam.cornell.edu>
2024-05-04 17:25:08 -0400

    in a class

--- examples/mnist/daemon.py
+++ examples/mnist/daemon.py
@@ -4,38 +4,74 @@ import time
 import logging
 import threading
 import multiprocessing
+import requests
 
-config = {
-    'batch_size': 64,
-    'epochs': 14,
-    'lr': 1.0,
-    'log_interval': 10
-}
+async def get_change():
+    while True:
+        res = await requests.get('http://10.48.85.83:8000/api/v1/jobs/<job_id>/get_change')
 
-def init(train):
-    # basic logging configurations
-    format = "%(asctime)s: %(message)s"
-    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+# async def create_new_job():
+#     while True:
+#         pass
 
-    # puts the training function into a process
-    logging.info("Creating training process")
-    train_process = multiprocessing.Process(target=train, args=(config,))
+# async def send_update_data():
+#     while True:
+#         pass
 
-    # start training process
-    logging.info("Starting training")
-    train_process.start()
+class Daemon:
+    def __init__(self, train):
+        # basic logging configurations
+        format = "%(asctime)s: %(message)s"
+        logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+        
+        self.train_process = None  # process for training loop
+        self.train_function = train  # target training function
+        self.train_config = {
+            'batch_size': 64,
+            'epochs': 14,
+            'lr': 1.0,
+            'log_interval': 10
+        }
 
-    async def stop_training(train_process):
+        event_loop = asyncio.get_event_loop()
+
+        try:
+            self.start_training()
+            # asyncio.ensure_future(self.start_training())
+            asyncio.ensure_future(self.stop_training())
+            event_loop.run_forever()
+        finally:
+            print("Closing Loop")
+            event_loop.close()
+
+    def start_training(self):
+        # puts the training function into a process
+        logging.info("Creating training process")
+        self.train_process = multiprocessing.Process(
+            target=self.train_function, 
+            args=(self.train_config,),
+            daemon=True
+        )
+
+        payload = {
+            "name": "thailand", 
+            "wandb": False,
+            "wandb_link": "",
+            "start_time": 1,
+            "config": self.train_config,
+            "cluster_name": "local",
+            "total_epoch": 14,
+        }
+
+        res = requests.post('http://10.48.85.83:8000/api/v1/jobs/new_job', json=payload)
+        print(res.text)
+
+        # start training process
+        logging.info("Starting training")
+        self.train_process.start()
+
+    async def stop_training(self):
         while True:
             await asyncio.sleep(5)
-            train_process.terminate()
-
-    # event loop for api requests
-    event_loop = asyncio.get_event_loop()
-
-    try:
-        asyncio.ensure_future(stop_training(train_process))
-        event_loop.run_forever()
-    finally:
-        print("Closing Loop")
-        event_loop.close()
+            if self.train_process:
+                self.train_process.terminate()
